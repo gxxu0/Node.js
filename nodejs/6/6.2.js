72bd72d6faee9e8bdd60d2898455418310f6da9e
gxxu0 <94434730+gxxu0@users.noreply.github.com>
2023-01-06 01:29:32 +0900

    Add files via upload

---
+++ nodejs/6/6.2.js
@@ -0,0 +1,8 @@
+console.log(1 + 1);
+console.log("1" + "1");
+console.log("1 + 1");
+
+console.log(
+    "Hoo ooh ooh ooh ooh Hoo ooh ooh ooh Stay in the middle Like you a little"
+        .length
+);
---
+++ nodejs/7/7.1.js
@@ -0,0 +1,7 @@
+a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+// 1 = 2; //오류
+
+var a = 1; //var 붙이기. 
