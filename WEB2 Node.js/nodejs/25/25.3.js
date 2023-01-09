d6225581409c0edbc08b9f19dc4c5ca3f7907461
gxxu0 <94434730+gxxu0@users.noreply.github.com>
2023-01-09 22:57:13 +0900

    Add files via upload

---
+++ WEB2 Node.js/nodejs/25/25.3.js
@@ -0,0 +1,8 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+console.log(`두 수의 합 : ${sum(4, 6)}`);
+
+Math.round(1.6); //출력하려면 console.log로 해야함.
+//filewrite("result.txt", Math.round(1.6)); //와 같은 형태로 보낼 수 있음.
+//더 광범위하게 보낼 수 있음.
